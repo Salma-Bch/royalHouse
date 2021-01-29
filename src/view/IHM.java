3fd827c6cc81ce2190c80c11a9a80476e8b5506c
S-BCH <S-BCH@users.noreply.github.com>
2021-01-29 16:46:15 +0100

    Troisième actualisation de l'IHM graphique.

--- src/view/IHM.java
+++ src/view/IHM.java
@@ -11,16 +11,12 @@ public class IHM extends JFrame {
 
     public static void main(String[] args) {
 
-        BoiteOutil boiteOutil =  new BoiteOutil() ;
-        ArrayList<Mobilier[]> mobilier = new ArrayList();
-     //   mobilier.add(new Mobilier("Canapé", "Baroque", "Canapé baroque blanc", 200, 200, false, "./ressources/images/canapeVuDenFace.png"));
-      //  mobilier.add(new Mobilier("Table", "Classique", "Table classique en bois", 200, 200, false, "./ressources/images/tableVuDenFace.png"));
-        JPanel panel2, panel3, panel4, panel5;
+        JPanel panel2, panel3, panel4;
         Toolbox panel1 = new Toolbox();
         panel2 = new JPanel();
         panel3 = new JPanel();
         panel4 = new JPanel();
-        GridPanel gridPanel = new GridPanel();
+        GridPanel panel5 = new GridPanel();
 
         panel1.setPreferredSize(new Dimension(275, 600));
         panel1.setBorder(BorderFactory.createEmptyBorder(200,20,20,200));
@@ -41,7 +37,7 @@ public class IHM extends JFrame {
         frame.add(panel2, BorderLayout.EAST);
         frame.add(panel3, BorderLayout.NORTH);
         frame.add(panel4, BorderLayout.SOUTH);
-        frame.add(gridPanel, BorderLayout.CENTER);
+        frame.add(panel5, BorderLayout.CENTER);
         frame.setVisible(true);
 
     }
--- src/view/Toolbox.java
+++ src/view/Toolbox.java
@@ -1,20 +1,31 @@
 package view;
 
+import model.BoiteOutil;
+import model.Mobilier;
 import model.Room;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Toolbox extends JPanel {
     public static int taille = 91 ;
     public static int width = 620;
     public static int length = 300;
     public static Room grid;
+    ArrayList<Mobilier> mobiliers;
+
+    BoiteOutil boiteOutil =  new BoiteOutil() ;
+
 
     public Toolbox() {
         //  setPreferredSize(new Dimension(601, 601));
         this.setSize(600, 600);
         grid = new Room();
+        mobiliers = new ArrayList<Mobilier>();
+        mobiliers.add(new Mobilier("Canapé", "Baroque", "Canapé baroque blanc", 200, 200, false, new ImageIcon("./ressources/images/canapeVuDenFace.png")));
+        mobiliers.add(new Mobilier("Table", "Classique", "Table classique en bois", 200, 200, false,  new ImageIcon("./ressources/images/tableVuDenFace.png")));
+        boiteOutil.setMobiliers(mobiliers);
     }
 
     @Override
@@ -22,6 +33,11 @@ public class Toolbox extends JPanel {
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
         drawGrid(g, width, taille,length);
+        for(int i=0; i< mobiliers.size(); i++){
+            Image image = mobiliers.get(i).getImage().getImage().getScaledInstance(91,80,Image.SCALE_DEFAULT);
+            ImageIcon imageIcon = new ImageIcon(image);
+            imageIcon.paintIcon(this, g2, i*91,1);
+        }
     }
 
     private void drawGrid(Graphics g, int width, int taille ,int length) {
