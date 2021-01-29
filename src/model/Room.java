639d4ebd7655113e39b206a0e46b0108ca3784fa
cerini <enzo.cerini@etu.u-cergy.fr>
2021-01-29 15:44:08 +0100

    room + gride

---
+++ src/model/Room.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Room {
+
+    public Room(){
+
+    }
+    //largeur
+    //longueur
+    //liste de mobiliers
+
+
+}
---
+++ src/view/GridPanel.java
@@ -0,0 +1,46 @@
+package view;
+
+import model.Room;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class GridPanel extends JPanel {
+    public static int taille = 50 ;
+    public static int width = 600;
+    public static int length = 600;
+    public static Room grid;
+
+    public GridPanel() {
+      //  setPreferredSize(new Dimension(601, 601));
+        this.setSize(600, 600);
+        grid = new Room();
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        drawGrid(g, width, taille,length);
+    }
+
+    private void drawGrid(Graphics g, int width, int taille ,int length) {
+        g.drawLine(length, 0, width, width);
+        g.drawLine(0, width, width, width);
+        g.drawLine(0, 0, 0, width);
+        g.drawLine(0, 0, width, 0);
+        g.setColor(Color.BLACK);
+
+        for (int i = 0; i <= width; i += taille) {
+            g.drawLine(i, 0, i, width);
+        }
+
+        for (int i = 0; i <= width; i += taille) {
+            g.drawLine(1, i, width, i);
+        }
+    }
+
+
+}
