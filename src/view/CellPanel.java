1f469456488cfdd88c6f628e233efedc780d53cf
MOUNCIF <Mons-L@users.noreply.github.com>
2021-03-05 16:45:08 +0100

    mise a jour de CellPanel

--- src/view/CellPanel.java
+++ src/view/CellPanel.java
@@ -5,6 +5,10 @@ import model.Furniture;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureRecognizer;
+import java.awt.dnd.DragSource;
+import java.awt.dnd.DropTarget;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -12,6 +16,11 @@ public class CellPanel extends JPanel implements MouseListener {
     private final Cell cell;
     private final int cellSize;
     private Color backgroundColor = Color.GRAY;
+
+    private DragGestureRecognizer dragGestureRecognizer;
+    private DragGestureHandler dragGestureHandler;
+    DropTarget dropTarget;
+    DropHandler dropHandler;
 
     public CellPanel(Cell cell, int cellSize){
         super();
@@ -23,7 +32,35 @@ public class CellPanel extends JPanel implements MouseListener {
        /* Furniture f1 = new Furniture("Canapé", "Baroque", "Canapé baroque bleu",
                 200, 200, false, new ImageIcon("./ressources/images/canapes/baroque_1.png"));
         cell.setFurniture(f1);*/
+
+        dropHandler = new DropHandler();
+        dropTarget = new DropTarget(this, DnDConstants.ACTION_COPY,dropHandler,true);
+        dragGestureHandler = new DragGestureHandler(this);
+        dragGestureRecognizer = DragSource.getDefaultDragSource().createDefaultDragGestureRecognizer(this, DnDConstants.ACTION_COPY, dragGestureHandler);
     }
+    @Override
+    public void addNotify(){
+        super.addNotify();
+       /* if (dgr == null){
+            dragGestureHandler = new DragGestureHandler(this);
+            dgr = DragSource.getDefaultDragSource().createDefaultDragGestureRecognizer(this
+            , DnDConstants.ACTION_COPY,dragGestureHandler);
+        }*/
+        dropHandler = new DropHandler();
+        dropTarget = new DropTarget(this, DnDConstants.ACTION_COPY, dropHandler, true);
+    }
+
+    @Override
+    public void removeNotify(){
+       /* if (dgr != null){
+            dgr.removeDragGestureListener(dragGestureHandler);
+            dragGestureHandler = null;
+        }
+        dgr = null;*/
+        super.removeNotify();
+        dropTarget.removeDropTargetListener(dropHandler);
+    }
+
 
     @Override
     public void paint(Graphics g){
