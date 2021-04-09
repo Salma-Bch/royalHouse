f0782c10f415ce541e5ab956cd6575244d92b902
RayaneDndoune <rayane.dendoune@gmail.com>
2021-04-09 15:45:24 +0200

    commentaires ajoutés

--- src/view/GridPanel.java
+++ src/view/GridPanel.java
@@ -6,6 +6,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * \file GridPanel.java
+ * \brief Classe qui permet d'afficher la grille correspondant à la zone de conception.
+ * \author BENCHELKA Salma, LEKMITI Mouncif, MANOUBI Farah, OBEYESEKARA Avishka, CERINI Enzo, DENDOUNE Rayane
+ * \version 1.0
+ * \date 09/04/2021
+ *
+ * Classe contenant toutes les fonctions permettant d'afficher la grille centrale.
+ */
+
 public class GridPanel extends JPanel  {
 
     private ArrayList<CellPanel> cellPanels;
@@ -14,6 +24,14 @@ public class GridPanel extends JPanel  {
     private boolean dropable;
     private int cellSize = 150;
 
+    /**
+     * GridPanel constructor
+     * @param columnNumber
+     * @param rowNumber
+     * @param dropable
+     * @param canThrow
+     * @param cellSize
+     */
     public GridPanel( int columnNumber, int rowNumber, boolean dropable, boolean canThrow, int cellSize) {
         super();
         this.dropable = dropable;
@@ -24,12 +42,21 @@ public class GridPanel extends JPanel  {
         cellPanels = initCellPanels(canThrow);
     }
 
+    /**
+     * @brief Fonction qui permet de réinitialiser les CellPanel.
+     * @details Cette fonction parcourt une Arraylist de CellPanel et remet la couleur de ces CellPanel à leur couleur de base (Color.GRAY)
+     */
     public void reinitCellPanels(){
         for (CellPanel cellPanel : cellPanels) {
             cellPanel.setBackgroundColor(Color.GRAY);
         }
     }
 
+    /**
+     * @brief Cette fonction permet d'initialiser les CellPanel.
+     * @param canThrow
+     * @return cellPanels Une ArrayList de CellPanel.
+     */
     public ArrayList<CellPanel> initCellPanels(boolean canThrow){
         ArrayList<CellPanel> cellPanels = new ArrayList<CellPanel>();
         int rows = grid.getRowNumber();
@@ -42,11 +69,19 @@ public class GridPanel extends JPanel  {
     }
 
 
+    /**
+     * @brief Fonction qui permet de dessiner.
+     * @param g
+     */
     @Override
     public void paint(Graphics g) {
         super.paint(g);
     }
 
+    /**
+     * @brief Fonction qui retourne le GridHandler.
+     * @return gridHandler Un GridHandler.
+     */
     public GridHandler getGridHandler() {
         return gridHandler;
     }
--- src/view/PanelDataFlavor.java
+++ src/view/PanelDataFlavor.java
@@ -3,6 +3,14 @@ package view;
 import javax.swing.*;
 import java.awt.datatransfer.DataFlavor;
 
+/**
+ * \file PanelDataFlavor.java
+ * \brief Classe qui est utile pour le système de "drag and drop".
+ * \author BENCHELKA Salma, LEKMITI Mouncif, MANOUBI Farah, OBEYESEKARA Avishka, CERINI Enzo, DENDOUNE Rayane
+ * \version 1.0
+ * \date 09/04/2021
+ */
+
 public class PanelDataFlavor extends DataFlavor {
 
     public static final PanelDataFlavor SHARED_INSTANCE = new PanelDataFlavor();
